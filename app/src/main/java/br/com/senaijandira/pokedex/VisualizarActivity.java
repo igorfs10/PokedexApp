ba9e264f846c93ae25e3f8047b497f14334230e4
LucasDMS <lucasdelmondes14@live.com>
2018-11-28 09:48:31 -0200

    Base stats adicionados

--- app/src/main/java/br/com/senaijandira/pokedex/VisualizarActivity.java
+++ app/src/main/java/br/com/senaijandira/pokedex/VisualizarActivity.java
@@ -11,6 +11,7 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 import br.com.senaijandira.pokedex.model.Pokemon;
+import br.com.senaijandira.pokedex.model.Stat;
 import br.com.senaijandira.pokedex.model.Type;
 import br.com.senaijandira.pokedex.presenter.VisualizarPresenter;
 import br.com.senaijandira.pokedex.service.ServiceFactory;
@@ -21,8 +22,9 @@ public class VisualizarActivity extends AppCompatActivity implements VisualizarV
     StringUtil util = new StringUtil();
     VisualizarPresenter presenter;
     ImageView imgPokemon;
-    TextView txtNome, txtId, txtTipo1, txtTipo2;
+    TextView txtNome, txtId, txtTipo1, txtTipo2, txtHp, txtSpeed, txtAtk, txtDef, txtSpAtk, txtSpDef;
     List<Type> tipos;
+    List<Stat> stats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +35,15 @@ public class VisualizarActivity extends AppCompatActivity implements VisualizarV
         txtId = findViewById(R.id.txtId);
         txtTipo1 = findViewById(R.id.txtTipo1);
         txtTipo2 = findViewById(R.id.txtTipo2);
+        txtHp = findViewById(R.id.txtHp);
+        txtSpeed = findViewById(R.id.txtSpeed);
+        txtAtk = findViewById(R.id.txtAtk);
+        txtDef = findViewById(R.id.txtDef);
+        txtSpAtk = findViewById(R.id.txtSpAtk);
+        txtSpDef = findViewById(R.id.txtSpDef);
+
+
+
 
         int idPokemon = getIntent().getIntExtra("idPoke", 0);
         Log.d("ID_POKEMON", idPokemon+"");
@@ -45,6 +56,7 @@ public class VisualizarActivity extends AppCompatActivity implements VisualizarV
     public void preencherPokemon(Pokemon pokemon){
         Picasso.get().load(pokemon.getSprites().getFront_default()).into(imgPokemon);
         tipos = pokemon.getTypes();
+        stats = pokemon.getStats();
         txtNome.setText(util.primeiraMaiuscula(pokemon.getSpecies().getName()));
         txtId.setText(pokemon.getId()+"");
         if(tipos.get(0).getSlot() == 2){
@@ -54,6 +66,13 @@ public class VisualizarActivity extends AppCompatActivity implements VisualizarV
             txtTipo1.setText(util.primeiraMaiuscula(tipos.get(0).getType().getName()));
             txtTipo2.setText("");
         }
+        txtHp.setText(stats.get(5).getBase_stat()+"");
+        txtSpeed.setText(stats.get(0).getBase_stat()+"");
+        txtAtk.setText(stats.get(4).getBase_stat()+"");
+        txtDef.setText(stats.get(3).getBase_stat()+"");
+        txtSpAtk.setText(stats.get(2).getBase_stat()+"");
+        txtSpDef.setText(stats.get(1).getBase_stat()+"");
+
     }
 
 
